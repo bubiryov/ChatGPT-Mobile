4f0f65e13dd4a0db6f34483bbce2d9c64f89d949
Egor <bubiryov@icloud.com>
2023-04-08 16:09:33 +0300

    Initial Commit

---
+++ ChatGPT Mobile/ChatGPT_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_MobileApp.swift
+//  ChatGPT Mobile
+//
+//  Created by Egor Bubiryov on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
